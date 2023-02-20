21b91d8ef7ef1566e67be7f4ebc07defae156c43
thitiphum-bluesage <123978441+thitiphum-bluesage@users.noreply.github.com>
2023-02-20 21:16:00 +0700

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -9,23 +9,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIF8UhTpqgwdZOv9E2Gps_Mv1uEmdt-hM",
-
-  authDomain: "realman-7cc75.firebaseapp.com",
-
-  databaseURL: "https://realman-7cc75-default-rtdb.asia-southeast1.firebasedatabase.app",
-
-  projectId: "realman-7cc75",
-
-  storageBucket: "realman-7cc75.appspot.com",
-
-  messagingSenderId: "94362129273",
-
-  appId: "1:94362129273:web:922e06944a5abdda7295a8",
-
-  measurementId: "G-BVB1ERHBP3"
-
-
+  xxx
 };
 
 // Initialize Firebase
